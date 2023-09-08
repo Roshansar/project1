41d287f64cb8cd4e3df6430c7762210caecdfc91
Roshansar <144414914+Roshansar@users.noreply.github.com>
2023-09-08 11:09:28 +0530

    Add files via upload

---
+++ rosh.py
@@ -0,0 +1,21 @@
+Name="Roshni"
+Age=19
+Dept="Computer Science"
+SSLC_M1=90
+SSLC_M2=91
+SSLC_M3=90
+SSLC_M4=90
+SSLC_M5=96
+SSLC_Total=SLC_M1+SSLC_M2+SSLC_M3+SSLC_M4+SSLC_M5
+SSLC_Avg=SSLC_Total/5
+HSC_Tam=90
+HSC_Eng=91
+HSC_Eco=90
+HSC_Comm=90
+HSC_Acc=96
+HSC_CA=99
+HSC_Total=HSC_Tam+HSC_Eng+HSC_Eco+HSC_Comm+HSC_Acc+HSC_CA
+HSC_Avg=HSC_Total/6
+Print("I'm ",Name,"and I'm ",Age,"years old.""from the dept of ",Dept,"SSLC Mark avg",SSLC_Avg,"HSC Marks avg",HSC_Avg) 
+
+
